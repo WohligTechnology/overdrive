a1c7b815f55ea7175b52264c9e1d4b155648c95a
Mahesh Nadar <mahesh.nadar@wohlig.com>
2017-10-13 19:17:07 +0530

    front page

--- frontend/js/controllers/controller.js
+++ frontend/js/controllers/controller.js
@@ -3,12 +3,70 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
         TemplateService.title = "Home"; //This is the Title of the Website
         $scope.navigation = NavigationService.getNavigation();
 
-        $scope.mySlides = [
-            'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
-            'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
-            'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
-            'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
-        ];
+        $scope.mySlides = [{
+            url: 'img/slider/1.png',
+            name: "name1"
+        }, {
+            url: 'img/slider/2.png',
+            name: "name2"
+        }, {
+            url: 'img/slider/3.png',
+            name: "name3"
+        }];
+
+
+
+        $scope.myWinner = [{
+            url: 'img/slider/car1.jpg',
+            name: "BEST CAR"
+        }, {
+            url: 'img/slider/car2.jpg',
+            name: "BEST BIKE"
+        }, {
+            url: 'img/slider/car3.jpg',
+            name: "BEST MUV"
+        }, {
+            url: 'img/slider/car4.jpg',
+            name: "BEST SUV"
+        }];
+
+
+        $scope.slider_profile = [{
+            url: 'img/slider/profile.jpg',
+            name: "Bertand Dsouza"
+        }, {
+            url: 'img/slider/profile1.jpg',
+            name: "Armaan Ibrahim"
+        }, {
+            url: 'img/slider/profile2.jpg',
+            name: "Anand Dharmaraj"
+        }, {
+            url: 'img/slider/profile3.jpg',
+            name: "Narayan Rupani"
+        }, ];
+
+
+        $scope.gallerytabs = [{
+                title: "2016",
+                gallery: ['img/gallery/2016/gal1.jpg', 'img/gallery/2016/gal2.jpg', 'img/gallery/2016/gal3.jpg', 'img/gallery/2016/gal4.jpg', 'img/gallery/2016/gal5.jpg', 'img/gallery/2016/gal6.jpg']
+            },
+            {
+                title: "2015",
+                gallery: ['img/gallery/2016/gal6.jpg', 'img/gallery/2016/gal4.jpg']
+            },
+            {
+                title: "2014",
+                gallery: ['img/gallery/2016/gal6.jpg', 'img/gallery/2016/gal4.jpg']
+            },
+            {
+                title: "2013",
+                gallery: ['img/gallery/2016/gal6.jpg', 'img/gallery/2016/gal4.jpg']
+            },
+            {
+                title: "2012",
+                gallery: ['img/gallery/2016/gal6.jpg', 'img/gallery/2016/gal4.jpg']
+            }
+        ]
         var abc = _.times(100, function (n) {
             return n;
         });
--- frontend/js/factories/navigation.js
+++ frontend/js/factories/navigation.js
@@ -1,23 +1,48 @@
 myApp.factory('NavigationService', function () {
     var navigation = [{
-            name: "Home",
+            name: "THE AWARDS",
             classis: "active",
             anchor: "home",
-            subnav: [{
-                name: "Subnav1",
-                classis: "active",
-                anchor: "home"
-            }]
+            subnav: []
+            // subnav: [{
+            //     name: "Subnav1",
+            //     classis: "active",
+            //     anchor: "home"
+            // }]
         }, {
-            name: "Form",
+            name: "AWARDS CATEGORIES",
             classis: "active",
             anchor: "form",
             subnav: []
         },
         {
-            name: "Grid",
+            name: "JURORS",
+            classis: "active",
+            anchor: "form",
+            subnav: []
+        }, {
+            name: "WINNERS",
+            classis: "active",
+            anchor: "form",
+            subnav: []
+        },
+        {
+            name: "GALLERY",
             classis: "active",
             anchor: "grid",
+            subnav: []
+        }, ,
+        {
+            name: "GAMES",
+            classis: "active",
+            anchor: "grid",
+            subnav: []
+        }, ,
+        {
+            name: "VOTE NOW",
+            classis: "active",
+            anchor: "grid",
+            highlight: true,
             subnav: []
         }
     ];
