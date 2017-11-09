c20732439af6d164b2a6f4514a4a2fe854ad17ce
Mahesh Nadar <mahesh.nadar@wohlig.com>
2017-11-09 17:53:27 +0530

    animation change

--- frontend/files.js
+++ frontend/files.js
@@ -25,6 +25,7 @@ module.exports = [
   "bower_components/angular-toastr/dist/angular-toastr.tpls.js",
   "bower_components/angular-promise-buttons/dist/angular-promise-buttons.js",
   "bower_components/angular-scroll/angular-scroll.js",
+  'bower_components/swiper/dist/js/swiper.min.js',
 
   //App.js
   'frontend/js/app.js',
