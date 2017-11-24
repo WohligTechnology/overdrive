46f9833052d7dbd4040db5384e9298a2452fe721
sayalighule <sayali.ghule@wohlig.com>
2017-11-24 17:43:21 +0530

    gallary year sort

--- api/services/Gallery.js
+++ api/services/Gallery.js
@@ -23,6 +23,7 @@ schema.plugin(uniqueValidator);
 schema.plugin(timestamps);
 module.exports = mongoose.model('Gallery', schema);
 
-var exports = _.cloneDeep(require("sails-wohlig-service")(schema, "order", "asc"));
+// var exports = _.cloneDeep(require("sails-wohlig-service")(schema, "year", "asc"));
+var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
 var model = {};
 module.exports = _.assign(module.exports, exports, model);
