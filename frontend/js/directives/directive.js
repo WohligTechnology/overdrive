6eafdff594c24e37edf18c9ac0256398836638c3
Mahesh Nadar <mahesh.nadar@wohlig.com>
2017-11-30 14:04:45 +0530

    ui changes

--- frontend/js/directives/directive.js
+++ frontend/js/directives/directive.js
@@ -1,24 +1,24 @@
 myApp.directive('img', function ($compile, $parse) {
-    return {
-        restrict: 'E',
-        replace: false,
-        link: function ($scope, element, attrs) {
-            var $element = $(element);
-            if (!attrs.noloading) {
-                $element.after("<img src='img/loading.gif' class='loading' />");
-                var $loading = $element.next(".loading");
-                $element.load(function () {
-                    $loading.remove();
-                    $(this).addClass("doneLoading");
-                });
-            } else {
-                $($element).addClass("doneLoading");
+        return {
+            restrict: 'E',
+            replace: false,
+            link: function ($scope, element, attrs) {
+                var $element = $(element);
+                if (!attrs.noloading) {
+                    $element.after("<img src='img/loading.gif' class='loading' />");
+                    var $loading = $element.next(".loading");
+                    $element.load(function () {
+                        $loading.remove();
+                        $(this).addClass("doneLoading");
+                    });
+                } else {
+                    $($element).addClass("doneLoading");
+                }
             }
-        }
-    };
-})
+        };
+    })
 
-.directive('hideOnScroll', function ($document) {
+    .directive('hideOnScroll', function ($document) {
         return {
             restrict: 'EA',
             replace: false,
@@ -87,74 +87,81 @@ myApp.directive('img', function ($compile, $parse) {
                     if ($(this).scrollTop() > 3500) {
                         $('.blade4').css("height", '1000px');
                     }
+
+                    if (($(this).scrollTop() + $(this).height()) > $('.footer').offset().top) {
+                        console.log("bottom reached", $('.footer').scrollTop())
+                        $('.nav_highlight').addClass('reached-bottom');
+                    } else {
+                        $('.nav_highlight').removeClass('reached-bottom');
+                    }
                 });
             }
         };
     })
 
 
-.directive('fancybox', function ($document) {
-    return {
-        restrict: 'EA',
-        replace: false,
-        link: function (scope, element, attr) {
-            var $element = $(element);
-            var target;
-            if (attr.rel) {
-                target = $("[rel='" + attr.rel + "']");
-            } else {
-                target = element;
+    .directive('fancybox', function ($document) {
+        return {
+            restrict: 'EA',
+            replace: false,
+            link: function (scope, element, attr) {
+                var $element = $(element);
+                var target;
+                if (attr.rel) {
+                    target = $("[rel='" + attr.rel + "']");
+                } else {
+                    target = element;
+                }
+
+                target.fancybox({
+                    openEffect: 'fade',
+                    closeEffect: 'fade',
+                    closeBtn: true,
+                    padding: 0,
+                    helpers: {
+                        media: {}
+                    }
+                });
             }
+        };
+    })
 
-            target.fancybox({
-                openEffect: 'fade',
-                closeEffect: 'fade',
-                closeBtn: true,
-                padding: 0,
-                helpers: {
-                    media: {}
-                }
-            });
-        }
-    };
-})
-
-.directive('autoHeight', function ($compile, $parse) {
-    return {
-        restrict: 'EA',
-        replace: false,
-        link: function ($scope, element, attrs) {
-            var $element = $(element);
-            var windowHeight = $(window).height();
-            $element.css("min-height", windowHeight);
-        }
-    };
-})
+    .directive('autoHeight', function ($compile, $parse) {
+        return {
+            restrict: 'EA',
+            replace: false,
+            link: function ($scope, element, attrs) {
+                var $element = $(element);
+                var windowHeight = $(window).height();
+                $element.css("min-height", windowHeight);
+            }
+        };
+    })
 
 
-.directive('replace', function () {
-    return {
-        require: 'ngModel',
-        scope: {
-            regex: '@replace',
-            with: '@with'
-        },
-        link: function (scope, element, attrs, model) {
-            model.$parsers.push(function (val) {
-                if (!val) {
-                    return;
-                }
-                var regex = new RegExp(scope.regex);
-                var replaced = val.replace(regex, scope.with);
-                if (replaced !== val) {
-                    model.$setViewValue(replaced);
-                    model.$render();
-                }
-                return replaced;
-            });
-        }
-    };
-})
+    .directive('replace', function () {
+        return {
+            require: 'ngModel',
+            scope: {
+                regex: '@replace',
+                with: '@with'
+            },
+            link: function (scope, element, attrs, model) {
+                model.$parsers.push(function (val) {
+                    if (!val) {
+                        return;
+                    }
+                    var regex = new RegExp(scope.regex);
+                    var replaced = val.replace(regex, scope.with);
+                    if (replaced !== val) {
+                        model.$setViewValue(replaced);
+                        model.$render();
+                    }
+                    return replaced;
+                });
+            }
+        };
+    })
 
 
 ;
